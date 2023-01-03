cd2900026617ccb0d6858dfa7c470f430a7af7f8
araschang0827 <b09704076@ntu.edu.tw>
2023-01-03 17:00:51 +0800

    test

--- Routes/BacktestController.py
+++ Routes/BacktestController.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse
 from rq import Queue
 from Base.Connector import RedisConnector
 from Base.ResponseCode import ResponseCode
+from redis import Redis, ConnectionPool
 
 class BacktestController(Resource):
     def get(self, exchange, symbol, timeframe):

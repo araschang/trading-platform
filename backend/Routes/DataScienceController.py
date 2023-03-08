26fcf4add585a0fe3877afd960b329941ef9bafa
araschang0827 <b09704076@ntu.edu.tw>
2023-03-08 22:53:14 +0800

    Background scheduling complete.

--- backend/Routes/DataScienceController.py
+++ backend/Routes/DataScienceController.py
@@ -1,5 +1,7 @@
+import pandas as pd
+import os
+import json
 from flask_restful import Resource
-from flask import request
 from Base.Connector import MongoConnector
 from Base.ResponseCode import ResponseCode
 
@@ -9,9 +11,27 @@ class SentimentalAnalysisController(Resource):
         mongo = MongoConnector()
         self._sentimentalAnalysisConnection = mongo.getSentimentalAnalysisConn()
     
+    def get(self):
+        '''
+        Get newest sentimental data
+        '''
+        path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Module/ML_Sentimental/sentiment_data.csv')
+        df = pd.read_csv(path)
+        value = df.iloc[-1, 1]
+        return value, ResponseCode.SUCCESS
+    
 
 class PriceNewsCrawlController(Resource):
     def __init__(self):
         mongo = MongoConnector()
         self._priceCrawlConnection = mongo.getPriceCrawlConn()
         self._newsCrawlConnection = mongo.getNewsCrawlConn()
+    
+    def get(self):
+        '''
+        Get news data.
+        '''
+        path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Module/PriceNews/result.json')
+        with open(path) as f:
+            j = json.load(f)
+        return j, ResponseCode.SUCCESS
